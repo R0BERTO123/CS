9050eecda23a86a402ba65c3adf865e388bed441
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-02 20:06:17 -0300

    02/08

---
+++ JurosCompostos/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é o valor total da compra?");
+        double valorCompra = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual é a quantidade de parcelas?");
+        int parcelas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual é a taxa de juros mensal em porcentagem?");
+        int taxa = int.Parse(Console.ReadLine());
+
+        double taxaJuros = taxa / 100.0;
+
+        double capital = valorCompra * Math.Pow(1 + taxaJuros, parcelas);
+        double final = capital / parcelas;
+
+        Console.WriteLine($"Sua compra ficará R$ {capital:F2} em {parcelas}x de R$ {final:F2} por mês.");
+    }
+}
