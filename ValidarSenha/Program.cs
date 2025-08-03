fa30a7a069c9f2c90ad05883a2601f52f49a113f
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-03 17:54:36 -0300

    03/08

---
+++ ValidarSenha/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a senha");
+        string senha = Console.ReadLine();
+
+        bool temNumero = false;
+        bool temEspecial = false;
+        string especial = "!@#$%^&*()_";
+
+        foreach (char c in senha)
+        {
+
+            if (char.IsDigit(c))
+            {
+                temNumero = true;
+            }
+
+            if (especial.Contains(c))
+            {
+                temEspecial = true;
+            }
+        }
+
+        bool senhaForte = senha.Length >= 6  && temNumero && temEspecial;     
+
+        Console.WriteLine($"A senha cumpre os requisitos? {senhaForte}");
+    }
+}
