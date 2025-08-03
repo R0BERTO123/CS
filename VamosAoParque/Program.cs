55705986302e50aa6e529657d6a8ebb38c90a432
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-03 15:44:37 -0300

    03/08

---
+++ VamosAoParque/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é previsão da temperatura do dia?");
+        double temperatura = double.Parse(Console.ReadLine());
+
+        bool apropriado = temperatura >= 23 || temperatura <= 30;
+
+        Console.WriteLine($"Vamos ao parque? {apropriado}");
+
+    }
+}
