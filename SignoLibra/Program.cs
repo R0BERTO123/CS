b4a4c3455261904314a134a5906571c079ad2019
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-03 15:46:10 -0300

    03/08

---
+++ SignoLibra/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Qual é o mês?");
+        string mes = Console.ReadLine();
+
+        Console.WriteLine("Qual é o dia?");
+        int dia = int.Parse(Console.ReadLine());
+
+        bool eDeLibra = false;
+
+        mes = mes.ToUpper();
+
+        if (mes.Equals("SETEMBRO") && dia >= 23)
+        {
+            eDeLibra = true;
+        }
+
+        else if (mes.Equals("OUTUBRO") && dia <= 22)
+        {
+            eDeLibra = true;
+       }
+
+        Console.WriteLine($"É do signo de Libra? {eDeLibra}");
+    }
+}
