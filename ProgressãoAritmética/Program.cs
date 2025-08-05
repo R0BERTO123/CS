9ddbf79eceb82a0c0b980d440db26bd52bfbcf07
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-05 17:59:51 -0300

    05/08

---
+++ ProgressãoAritmética/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Qual é o valor da primeira posição?");
+int a1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual é a razão?");
+int razao = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual é a posição desejada?");
+int posicao = int.Parse(Console.ReadLine());
+
+double termo = a1 * Math.Pow(razao, posicao - 1);
+
+Console.WriteLine($"O valor do termo na posição {posicao} é {termo}");
+
+
