82e71e72775c488b012895e27d9af9859491f65e
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-05 17:08:33 -0300

    05/08

---
+++ MegaSena/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+
+int numero1 = random.Next(1, 60);
+int numero2 = random.Next(1, 60);
+int numero3 = random.Next(1, 60);
+int numero4 = random.Next(1, 60);
+int numero5 = random.Next(1, 60);
+int numero6 = random.Next(1, 60);
+
+Console.WriteLine(numero1);
+Console.WriteLine(numero2);
+Console.WriteLine(numero3);
+Console.WriteLine(numero4);
+Console.WriteLine(numero5);
+Console.WriteLine(numero6);
