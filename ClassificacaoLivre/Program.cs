fb132e9eb7c7e161995d9e5aa8c228e1b9bf15fb
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-07 18:50:03 -0300

    07/08

---
+++ ClassificacaoLivre/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é o nome do nome do filme?");
+        string nomeFilme = Console.ReadLine();
+
+        Console.WriteLine("Qual é a classificação?");
+        int classificacaoFilme = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual é a idade da primera pessoa?");
+        int idadeP1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual é a idade da segunda pessoa?");
+        int idadeP2 = int.Parse(Console.ReadLine());
+
+        bool classificacaoLivre = true;
+
+        if (classificacaoFilme != 0)
+        {
+            classificacaoLivre = false;
+            if (idadeP1 >= classificacaoFilme && idadeP2 >= classificacaoFilme)
+            {
+                classificacaoLivre = true;
+            }
+        }
+
+        Console.WriteLine($"Vamos assistir um filme? {classificacaoLivre}");
+    }
+}
