4ff59b87918df57ae25cfe6458bd691588109e2c
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-07 18:31:25 -0300

    07/08

---
+++ CorPrimaria/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Digite uma cor");
+string cor = Console.ReadLine();
+cor = cor.ToLower();
+
+bool corPrimaria = cor == "azul" || cor == "vermelho" || cor == "amarelo";
+
+Console.WriteLine($"É cor primária? {corPrimaria}");
