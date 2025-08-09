689a0f430dc24004f67a06684f21f6a55635bd17
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-09 19:17:04 -0300

    09/08

---
+++ CPF/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu CPF");
+        string cpf = Console.ReadLine();
+
+        // cpf = cpf.Replace(" ", "").Replace(".", "").Replace("-","");
+        cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+        cpf = cpf.Insert(3, ".").Insert(7,".").Insert(11,"-");
+
+
+        Console.WriteLine(cpf);
+    }
+}
