e912380e65b5229688a026cb11567d8b086bca67
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-09 20:57:16 -0300

    09/08

---
+++ SenhaPadrao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é o nome?");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Qual é o CPF?");
+        string cpf = Console.ReadLine();
+        cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+        Console.WriteLine("Qual é o numero de telefone?");
+        string numero = Console.ReadLine();
+        numero = Regex.Replace(numero, "[^0-9]", "");
+
+        string primeroNome = nome.Split(" ")[0];
+        char primeiraLetraNome = primeroNome[0];
+
+        string sobrenome = nome.Split(" ")[2];
+        char primeiraLetraSobrenome = sobrenome[0];
+
+
+        string cpftres = cpf.Substring(0, 3);
+
+        string numeroDois = numero.Substring(5, 2);
+
+
+        Console.WriteLine($"Senha criada: {primeiraLetraNome}{primeiraLetraSobrenome}{cpftres}{numeroDois}");
+       
+       
+        
+    }
+}
