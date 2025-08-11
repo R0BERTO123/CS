bb8687b96f75908012b37d757d4956bd212ea1f9
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-11 18:46:25 -0300

    11/08

---
+++ Whatsapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é o numero?");
+        string numero = Console.ReadLine();
+
+        numero = Regex.Replace(numero, "[^0-9]", "");
+
+        Console.WriteLine($"https://api.whatsapp.com/send?phone={numero}");
+    }
+}
