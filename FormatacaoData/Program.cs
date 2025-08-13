f522a4b9bb02b4da73a6617eb563b0745017eb8f
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-13 17:26:13 -0300

    13/08

---
+++ FormatacaoData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a data é hora");
+        string dataHora = Console.ReadLine();
+
+        if (DateTime.TryParse(dataHora, out DateTime dataHoraConverte))
+        {
+
+            string formataDataHora = dataHoraConverte.ToString("dd/MM/yyyy");
+            Console.WriteLine(formataDataHora);
+        }
+
+        else
+        {
+             Console.WriteLine("erro");
+        }
+    
+       
+
+    }
+}
