2958f8aa909fa8d50d36fdb8cb56c19c44096b9a
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-13 17:52:13 -0300

    13/08

---
+++ PlataformaDeIdiomas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a data é hora");
+        string dataHora = Console.ReadLine();
+
+        Console.WriteLine("Digite o fuso");
+        int fuso = int.Parse(Console.ReadLine());
+
+        if (DateTime.TryParse(dataHora, out DateTime dataHoraConvertida))
+        {
+            DateTime dataHoraComFuso = dataHoraConvertida.AddHours(fuso);
+
+            string dateHoraFormatada = dataHoraComFuso.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine("Horário local: " + dateHoraFormatada);
+            
+        }
+    }
+}
