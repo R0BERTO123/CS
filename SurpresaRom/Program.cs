b32119ae6438aa010507d81a4635699c58e250a3
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-17 16:13:52 -0300

    17/08

---
+++ SurpresaRom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a date é hora");
+        string dataHora = Console.ReadLine();
+
+        if (DateTimeOffset.TryParse(dataHora, out DateTimeOffset dataHoraDT))
+        {
+            
+            TimeZoneInfo tzBrasil;
+            if (OperatingSystem.IsWindows())
+            {
+                tzBrasil = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            }
+            else
+            {
+             tzBrasil = TimeZoneInfo.FindSystemTimeZoneById("America/Sao_Paulo");
+            }
+
+
+            DateTimeOffset fusoBrasil = TimeZoneInfo.ConvertTime(dataHoraDT, tzBrasil);
+
+            string formata = fusoBrasil.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine($"Avise o hotel em {formata} de Brasil/SãoPaulo");
+        }
+    }
+}
