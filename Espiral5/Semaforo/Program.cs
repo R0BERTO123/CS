cad7c461a6e571a268cbf92082c2c5185b1d1cc7
RobertoDias <ra48273551857@acaonsfatima.org.br>
2026-05-31 18:28:47 -0300

    31/05

---
+++ Espiral5/Semaforo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static string Semaforo(string cor)
+    {
+        if (cor == "VERMELHO") return "Espere";
+
+        else if (cor == "VERDE") return "Atravesse";
+
+        else if (cor == "ROXO") return "Farol inoperante";
+
+        else return "Cor Não 🤓";
+
+        
+    }
+    public static void Main(string[] args)
+    {
+        string cor = Semaforo(Console.ReadLine().ToUpper());
+
+        Console.WriteLine(cor);
+    }
+}
