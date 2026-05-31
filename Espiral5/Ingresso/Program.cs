cb2ba9a029ce30a02faad99ddb1441fe3a5bab54
RobertoDias <ra48273551857@acaonsfatima.org.br>
2026-05-31 18:28:12 -0300

    31/05

---
+++ Espiral5/Ingresso/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    static double ValorIngresso(string t, int q)
+    {
+        double valor = 0;
+        if (t == "MEIA")
+        {
+            valor = (28.50 * q) / 2;
+            
+        }
+        else if (t == "INTEIRA")
+        {
+             valor = 28.50 * q;
+            
+        }
+        return valor;
+    }
+    public static void Main(string[] args)
+    {
+        string tipo = Console.ReadLine().ToUpper();
+        int quantidade = Convert.ToInt32(Console.ReadLine());
+
+        double valor = ValorIngresso(tipo, quantidade);
+
+        Console.WriteLine($"O total a pagar é R$ {valor:F2}");
+    }
+}
